979e4063b903b42e4a88ae37c54f6eb723334b64
breadbrad <yimdonghoon@gmail.com>
2016-10-11 05:18:06 +0000

    Add show action and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Gourmet::Application.routes.draw do
   root "foods#posts"
   get "/:category" => 'foods#posts_category'
   get "foods/posts_category"
-  get "foods/show"
+  get "foods/show/:id" => 'foods#show'
   get "foods/write"
   get "foods/write_complete"
   get "foods/edit"
