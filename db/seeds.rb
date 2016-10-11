6ce63fcde74f6652a96882906a2db726b175adb1
breadbrad <yimdonghoon@gmail.com>
2016-10-11 04:11:56 +0000

    Enter seed data post table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+categories = ["Korean", "Japanese", "Chinese", "American"]
+
+categories.each do |category|
+  0.upto(1) do |i|
+    p = Post.new
+    p.user_id = i + 1
+    p.category = category
+    p.title = "#{category}집#{i} 이름"
+    p.content = "#{category}집#{i} 설명"
+    p.save
+  end
+end
